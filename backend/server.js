96fcbee24b36c2827559cef89a18504d483e9f62
ridham-dd <147288551+ridham-dd@users.noreply.github.com>
2025-03-05 19:19:58 -0600

    cors updated

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,9 @@ const cors = require("cors");
 const { spawn } = require("child_process");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 app.get("/", (req, res)=>{
