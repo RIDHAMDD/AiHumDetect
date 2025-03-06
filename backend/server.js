8a1c6760620e458b932d327708ee45417579b428
ridham-dd <147288551+ridham-dd@users.noreply.github.com>
2025-03-05 19:12:44 -0600

    server running

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res)=>{
+  res.send("Server Running")
+})
+
 app.post("/predict", (req, res) => {
   const { model_name, content } = req.body;
   console.log("Received prediction request for model:", model_name);
